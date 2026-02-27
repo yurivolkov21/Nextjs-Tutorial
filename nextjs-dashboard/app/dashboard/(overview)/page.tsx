31d85ce1f77be642bb79d5eacf6dbe66dd6363f6
yurivolkov21 <yurivolkov1210@gmail.com>
2026-02-27 22:45:41 +0700

    feat: add middleware for NextAuth authentication in Next.js dashboard

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -3,6 +3,11 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
