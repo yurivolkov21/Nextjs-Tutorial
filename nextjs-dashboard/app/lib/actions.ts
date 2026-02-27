93b7875deae32fd40462899b8a4e671cf439a7b5
yurivolkov21 <yurivolkov1210@gmail.com>
2026-02-27 22:06:47 +0700

    feat: implement authentication flow with NextAuth and create login page

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -113,4 +115,23 @@ export async function deleteInvoice(id: string) {
     // Unreachable code block
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+    ) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+        switch (error.type) {
+            case 'CredentialsSignin':
+            return 'Invalid credentials.';
+            default:
+            return 'Something went wrong.';
+        }
+        }
+        throw error;
+    }
 }
